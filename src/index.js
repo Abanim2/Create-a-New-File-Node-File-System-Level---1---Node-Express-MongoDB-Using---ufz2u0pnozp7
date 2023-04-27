a8d756ddf05c3f1c8f5cad323fa70310bbfa2cce
Abanim2 <115773506+Abanim2@users.noreply.github.com>
2023-04-27 11:12:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+// const fs = require("fs/promises");
+// const fileName = "myfile.txt";
+// const fileContent = "Newton School";
+// const writeFile = async (fileName, fileContent) => {
+//   // write code here
+//   // dont change function name
+// };
+
+// module.exports = { writeFile };
+
+
+
+
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const fileContent = "Newton School";
+
 const writeFile = async (fileName, fileContent) => {
-  // write code here
-  // dont change function name
+  try {
+    // create the file with the given filename and content
+    await fs.writeFile(fileName, fileContent);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports = { writeFile };
